752b9e5414adf9ed854ec56c2920389ef968da3f
abir.mahmud <abir@socialcubebd.com>
2024-08-23 08:21:58 +0600

    transaction delete completed and ready to deploy vercel

--- app/(dashboard)/transactions/_components/DeleteTransactionDialog.tsx
+++ app/(dashboard)/transactions/_components/DeleteTransactionDialog.tsx
@@ -1,6 +1,22 @@
 "use client";
 
-import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { TransactionType } from "@/lib/types";
+import { Category } from "@prisma/client";
+import { AlertDialogTitle } from "@radix-ui/react-alert-dialog";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React, { ReactNode } from "react";
+import { toast } from "sonner";
+import { DeleteTransaction } from "../_actions/deleteTransaction";
 
 interface Props {
   open: boolean;
@@ -9,7 +25,52 @@ interface Props {
 }
 
 function DeleteTransactionDialog({ open, setOpen, transactionId }: Props) {
-  return <div></div>;
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: DeleteTransaction,
+    onSuccess: async () => {
+      toast.success("Transaction deleted successfully", {
+        id: transactionId,
+      });
+
+      await queryClient.invalidateQueries({
+        queryKey: ["transactions"],
+      });
+    },
+
+    onError: () => {
+      toast.success("Something went wrong", {
+        id: transactionId,
+      });
+    },
+  });
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. this will permanently delete this
+            transaction
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              toast.loading("Deleting transaction...", {
+                id: transactionId,
+              });
+              deleteMutation.mutate(transactionId);
+            }}
+          >
+            Proceed
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
 
 export default DeleteTransactionDialog;
